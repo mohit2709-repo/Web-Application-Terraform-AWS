cf70def9527c27e10b2040239371fd3bb00e3837
mohit2709-repo <sharma.mohit332@gmail.com>
2024-07-02 17:01:33 +0530

    Adding script and config files

---
+++ main.tf
@@ -0,0 +1,153 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "10.0.0.0/24"
+    availability_zone       = "us-east-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sub2" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "us-east-1b"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "mysg" {
+  name        = "web_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
+/*resource "aws_s3_bucket" "example" {
+  bucket = "tf04JunebucketTue"
+}*/
+
+resource "aws_instance" "webserver1" {
+  ami                       = "ami-04b70fa74e45c3917"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.mysg.id]
+  subnet_id                 = aws_subnet.sub1.id
+  user_data                 = base64encode(file("userdata.sh"))
+
+  tags = {
+    Name = "Created by TF"
+  }
+}
+
+resource "aws_instance" "webserver2" {
+  ami                       = "ami-04b70fa74e45c3917"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.mysg.id]
+  subnet_id                 = aws_subnet.sub2.id
+  user_data                 = base64encode(file("userdata1.sh"))
+
+  tags = {
+    Name = "Created by TF"
+  }
+}
+
+resource "aws_lb" "myalb" {
+  name               = "my-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.mysg.id]
+  subnets            = [aws_subnet.sub1.id,aws_subnet.sub2.id]
+
+  tags = {
+    Environment = "demo-TF"
+  }
+}
+
+resource "aws_lb_target_group" "mytg" {
+  name     = "my-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mytg.arn
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+
+}
